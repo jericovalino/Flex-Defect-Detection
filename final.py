b6b990beb541a3aec4f84ab72aab4fe368c35a6e
jerico valino <45051712+jericovalino@users.noreply.github.com>
2019-05-03 18:29:03 +0800

    Update final.py

--- final.py
+++ final.py
@@ -52,89 +52,89 @@ class Application:
         self.root = Tk()  # initialize root window
         self.root.overrideredirect(True)
 
-        self.saving_dir = CWD_PATH
-        bgColor = "#222"
+        self.SAVING_DIR = CWD_PATH
+        bg_color = "#222"
 
         # Gets both half the screen width/height and window width/height
-        screenLengthX = int(self.root.winfo_screenwidth()/2 - (843/2))
-        screenLengthY = int(self.root.winfo_screenheight()/2 - (680/2))
+        screen_length_x = int(self.root.winfo_screenwidth()/2 - (843/2))
+        screen_length_y = int(self.root.winfo_screenheight()/2 - (680/2))
 
-        self.root.geometry(f"843x680+{screenLengthX}+{screenLengthY}") # window position(center)
+        self.root.geometry(f"843x680+{screen_length_x}+{screen_length_y}") # window position(center)
 
         self.root.iconbitmap(default='icon.ico') # set icon
         self.root.title("Flex Defect Detection")  # set window title
         self.root.resizable(False,False)
-        self.root.configure(bg=bgColor) #bg color
+        self.root.configure(bg=bg_color) #bg color
         self.root.protocol('DELETE_WINDOW', self.destructor)
 
 
         """declairing variable for UI starts here"""
         self.topBar = Frame(self.root)
         self.blueLine = Frame(self.root, bg="#4285F4", height=7)
-        self.titleBar = Frame(self.root, bg=bgColor, height=30)
-        self.centerFrame = Frame(self.root, bg=bgColor)
-        self.terminalFrame = Frame(self.root, bg=bgColor, height=100)
+        self.titleBar = Frame(self.root, bg=bg_color, height=30)
+        self.centerFrame = Frame(self.root, bg=bg_color)
+        self.terminalFrame = Frame(self.root, bg=bg_color, height=100)
         self.terminalScrollBar = Scrollbar(self.terminalFrame, )
         self.terminalListBox = Listbox(self.terminalFrame, bg="#1c313a", fg="white", width=102, height=8, borderwidth=0,
             highlightthickness=0, font=('verdana', 10),  yscrollcommand=self.terminalScrollBar.set)
         self.terminalScrollBar.config(command=self.terminalListBox.yview)
         self.panel = Label(self.centerFrame, width = 91, height = 35, bg="#1c313a",
             text="No connected camera found!!! \nConnect a camera then restart the application.")  # initialize image panel
-        self.lblFrame = LabelFrame(self.centerFrame,text="MENU",background="#f5f5f5")
-        logoImg = PhotoImage(file = 'logo.PNG')
-        logo = Label(self.titleBar, image=logoImg, bd=0, bg=bgColor); logo.image = logoImg
+        self.menuFrame = LabelFrame(self.centerFrame,text="MENU",background="#f5f5f5")
+        logo_img = PhotoImage(file = 'logo.PNG')
+        logo = Label(self.titleBar, image=logo_img, bd=0, bg=bg_color); logo.image = logo_img
         """declairing variable for UI ends here"""
 
 
         """declaring variables for top bar buttons starts here"""
-        exitImg = PhotoImage(file = "red.PNG")
-        minImg = PhotoImage(file = "yellow.PNG")
-        maxImg = PhotoImage(file = "green.PNG")
-        exitbtn = Button(self.topBar, image=exitImg, bd=0, command=self.destructor); exitbtn.image = exitImg
-        maxbtn = Button(self.topBar, image=maxImg, bd=0); maxbtn.image = maxImg
-        minbtn = Button(self.topBar, image=minImg, bd=0, command=self.minimize); minbtn.image = minImg
+        exit_img = PhotoImage(file = "red.PNG")
+        min_img = PhotoImage(file = "yellow.PNG")
+        max_img = PhotoImage(file = "green.PNG")
+        exitButton = Button(self.topBar, image=exit_img, bd=0, command=self.destructor); exitButton.image = exit_img
+        maxButton = Button(self.topBar, image=max_img, bd=0, command=self.clear_terminal); maxButton.image = max_img
+        minButton = Button(self.topBar, image=min_img, bd=0, command=self.minimize); minButton.image = min_img
         """declaring variables for top bar buttons ends here"""
 
 
         """initializing dragging feature starts here"""
         self.drag = self.topBar
-        self.drag.bind('<ButtonPress-1>', self.StartMove)
-        self.drag.bind('<ButtonRelease-1>', self.StopMove)
-        self.drag.bind('<B1-Motion>', self.OnMotion)
+        self.drag.bind('<ButtonPress-1>', self.start_move)
+        self.drag.bind('<ButtonRelease-1>', self.stop_move)
+        self.drag.bind('<B1-Motion>', self.on_motion)
         """initializing dragging feature ends here"""
 
 
-        exitbtn.grid(column = 0, row = 0, padx=(3,0))
-        minbtn.grid(column = 1, row = 0, padx=(1,0))
-        maxbtn.grid(column = 2, row = 0, padx=(1,0))
+        exitButton.grid(column=0,row=0,padx=(3,0))
+        minButton.grid(column=1,row=0,padx=(1,0))
+        maxButton.grid(column=2,row=0,padx=(1,0))
 
         titleLabel = Label(self.titleBar, text="Cosmetic Quality Defect Detection in Electronics",
-            bd=0, bg=bgColor, foreground='#fefefe',font=('verdana', 10))
+            bd=0, bg=bg_color, foreground='#fefefe',font=('verdana', 10))
 
         self.topBar.grid(sticky=(W,E))        
         self.blueLine.grid(row=1,column=0,sticky=(W,E))
         self.titleBar.grid(row=2,column=0,sticky=(W,E))
-        logo.grid(row=0, column=0, pady=3); titleLabel.grid(row=0, column=1, padx=10)
-        self.centerFrame.grid(row=3, column=0,sticky=(W,E))
-        self.terminalFrame.grid(row=4, column=0,sticky=(W,E))
+        logo.grid(row=0,column=0,pady=3); titleLabel.grid(row=0, column=1, padx=10)
+        self.centerFrame.grid(row=3,column=0,sticky=(W,E))
+        self.terminalFrame.grid(row=4,column=0,sticky=(W,E))
         self.terminalListBox.grid(row=0,column=0,sticky=(N,E,S,W),padx=3,pady=3)
         self.terminalScrollBar.grid(row=0,column=1,sticky=(N,S,E),padx=(2,3),pady=3)
         self.panel.grid(row=0,column=0,sticky=W,padx=(3,0))
-        self.lblFrame.grid(row=0,column=1,sticky=(N,E,S,W),padx=(0,3))
+        self.menuFrame.grid(row=0,column=1,sticky=(N,E,S,W),padx=(0,3))
 
 
         # create a button, that when pressed, will take the current frame and save it to file
-        detectionButton = Button(self.lblFrame, text="Run Detection", command=self.take_snapshot)
-        detectionButton.configure(foreground="white", background="#4285F4", width=20, height=2, relief=GROOVE, font=('verdana', 10, 'bold') )
-        detectionButton.grid(column = 0, row = 1, sticky = (E,W))
+        detectionButton = Button(self.menuFrame,text="Run Detection",command=self.take_snapshot)
+        detectionButton.configure(foreground="white",background="#4285F4",width=20,height=2,relief=GROOVE,font=('verdana',10,'bold'))
+        detectionButton.grid(column=0,row=1,sticky=(E,W))
 
-        changeDirButton = Button(self.lblFrame, text="change saving directory", command=(self.change_saving_dir))
-        changeDirButton.grid(column = 0, row = 3, sticky = (E,W), pady=(375,0))
+        changeDirButton = Button(self.menuFrame, text="change saving directory", command=(self.change_saving_dir))
+        changeDirButton.grid(column=0,row=3,sticky=(E,W),pady=(375,0))
         
         # create a button, that when pressed, will take the current frame and save it to file
-        self.entr = Entry(self.lblFrame, text="evaluate")
+        self.entr = Entry(self.menuFrame, text="evaluate")
         self.entr.configure(bd=1, background="#9e9e9e")
-        self.entr.grid(column = 0, row = 0, sticky = (E,W))
+        self.entr.grid(column=0,row=0,sticky=(E,W))
 
 
         self.counter = 0
@@ -142,6 +142,7 @@ class Application:
 
         # start a self.video_loop that constantly pools the video sensor for the most recently read frame
         self.video_loop()
+        self.terminal_print("INFO", 'Click "Run Detection" to start.')
 
         self.root.bind('<Map>', self.check_map) # added bindings to pass windows status to function
         self.root.bind('<Unmap>', self.check_map)
@@ -151,20 +152,18 @@ class Application:
             self.root.overrideredirect(True)
     """function for minimize feature ends here"""
 
-
     """functions for dragging feature starts here"""
-    def StartMove(self, event):
+    def start_move(self, event):
         self.x = event.x
         self.y = event.y
-    def StopMove(self, event):
+    def stop_move(self, event):
         self.x = None
         self.y = None
-    def OnMotion(self, event):
+    def on_motion(self, event):
         x = (event.x_root - self.x - self.drag.winfo_rootx() + self.drag.winfo_rootx())
         y = (event.y_root - self.y - self.drag.winfo_rooty() + self.drag.winfo_rooty())
         self.root.geometry(f"+{x}+{y}")
     """functions for dragging feature ends here"""
-
 
     def video_loop(self):
         """ Get frame from the video stream and show it in Tkinter """
@@ -178,13 +177,16 @@ class Application:
         if not ok:
             if self.counter == 0:
                 self.counter += 1
-                self.terminalPrint("WARNING", "No connected camera found!!!")
+                self.terminal_print("WARNING", "No connected camera found!!!")
             self.cam = cv2.VideoCapture(0)
         self.root.after(30, self.video_loop)  # call the same function after 30 milliseconds
 
-    def terminalPrint(self, mtype, message):
+    def terminal_print(self, mtype, message):
         self.terminalListBox.insert(self.listCounter, f"[{mtype}] {message}")
         self.listCounter += 1
+
+    def clear_terminal(self):
+        self.terminalListBox.delete(0,END)
 
     def destructor(self):
         """ Destroy the root object and release all resources """
@@ -193,25 +195,23 @@ class Application:
         self.cam.release()  # release web camera
         cv2.destroyAllWindows()  # it is not mandatory in this application
 
-
     def minimize(self):
         self.root.wm_withdraw()
         self.root.wm_overrideredirect(False)
         self.root.wm_iconify()
-
 
     def load_image_into_numpy_array(self,image):
         (im_width, im_height) = image.size
         return np.array(image.getdata()).reshape(
             (im_height, im_width, 3)).astype(np.uint8)
 
-
     def run_inference(self,rawimage,image):
         with detection_graph.as_default():
             with tf.Session() as sess:
                 #os.system('mode con: cols=80 lines=4')
                 os.system('cls')
-                self.terminalPrint("INFO","Evaluating captured image. Please wait...")                
+                print("awesome!")
+                self.terminal_print("INFO","Evaluating captured image. Please wait...")                
                 # Get handles to input and output tensors
                 ops = tf.get_default_graph().get_operations()
                 all_tensor_names = {
@@ -244,7 +244,6 @@ class Application:
                         detection_masks_reframed, 0)
                 image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-
                 # Run inference
                 output_dict = sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)})
 
@@ -266,22 +265,22 @@ class Application:
                     instance_masks=output_dict.get('detection_masks'),
                     use_normalized_coordinates=True,
                     line_thickness=2)
-                self.terminalPrint("INFO","Done.")
+                self.terminal_print("INFO","Detection complete.")
                 cv2.imshow("Result", frame)
                 cv2.moveWindow("Result",self.root.winfo_x()-5,self.root.winfo_y()+23)
-
 
     def take_snapshot(self):
         """ Take snapshot and save it to the file """
         self.counter += 1
         ts = datetime.datetime.now() # grab the current timestamp
+        cv2.destroyWindow("Result")
         filename = "{}_Image_{}.jpg".format(ts.strftime("%Y_%m_%d"),self.counter)  # construct filename
         path = os.path.join(CWD_PATH, 'save_images', filename)  # construct output path
         image = cv2.cvtColor(numpy.array(self.current_image), cv2.COLOR_RGB2BGR)  # save image as jpeg file
         cv2.imwrite(path, image)
         result = self.entr.get()
-        message = "saved! {}".format(filename)
-        self.terminalPrint("INFO",message)
+        message = f" {filename}"
+        self.terminal_print("SAVE",message)
                     
         CAPTURED_IMAGE_PATH = os.path.join(SAVE_IMAGES_PATH, filename)
         imageopen = Image.open(CAPTURED_IMAGE_PATH)
@@ -291,8 +290,8 @@ class Application:
         self.run_inference(rawimage,image_np)
 
     def change_saving_dir(self):
-        self.saving_dir = filedialog.askdirectory()
-        self.terminalPrint("INFO",f"Saving Directory: {self.saving_dir}")
+        self.SAVING_DIR = filedialog.askdirectory()
+        self.terminal_print("PATH",f" {self.SAVING_DIR}")
 
 
 if __name__ == "__main__":
