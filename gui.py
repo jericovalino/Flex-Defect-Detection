73af4716a508ee6278af52f66c6367cce3b4b252
jerico valino <45051712+jericovalino@users.noreply.github.com>
2019-05-03 15:12:15 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from tkinter import filedialog
 from tkinter import *
 from PIL import Image, ImageTk
 #import argparse
@@ -18,6 +19,9 @@ class Application:
         self.root = Tk()  # initialize root window
         self.root.overrideredirect(True)
 
+        self.saving_dir = os.getcwd()
+        bgColor = "#222"
+
         # Gets both half the screen width/height and window width/height
         screenLengthX = int(self.root.winfo_screenwidth()/2 - (843/2))
         screenLengthY = int(self.root.winfo_screenheight()/2 - (680/2))
@@ -25,21 +29,25 @@ class Application:
         self.root.geometry(f"843x680+{screenLengthX}+{screenLengthY}") # window position(center)
         self.root.iconbitmap(default='icon.ico')        
         self.root.resizable(False,False)
-        self.root.configure(bg="#222222") #bg color
+        self.root.configure(bg=bgColor) #bg color
         self.root.protocol('DELETE_WINDOW', self.destructor)
 
 
         """declairing variable for UI starts here"""
         self.topBar = Frame(self.root)
         self.blueLine = Frame(self.root, bg="#4285F4", height=7)
-        self.titleBar = Frame(self.root, bg="#222222", height=30)
-        self.centerFrame = Frame(self.root, bg="#222222")
-        self.terminalFrame = Listbox(self.root, height=8, bg="#263238", borderwidth=0, highlightthickness=0, font=('verdana', 10), fg="white")
-        self.panel = Label(self.centerFrame, width = 91, height = 35,
+        self.titleBar = Frame(self.root, bg=bgColor, height=30)
+        self.centerFrame = Frame(self.root, bg=bgColor)
+        self.terminalFrame = Frame(self.root, bg=bgColor, height=100)
+        self.terminalScrollBar = Scrollbar(self.terminalFrame, )
+        self.terminalListBox = Listbox(self.terminalFrame, bg="#1c313a", fg="white", width=102, height=8, borderwidth=0,
+            highlightthickness=0, font=('verdana', 10),  yscrollcommand=self.terminalScrollBar.set)
+        self.terminalScrollBar.config(command=self.terminalListBox.yview)
+        self.panel = Label(self.centerFrame, width = 91, bg="#1c313a", fg="white",
             text="No connected camera found!!! \nConnect a camera then restart the application.")  # initialize image panel
         self.lblFrame = LabelFrame(self.centerFrame,text="MENU",background="#f5f5f5")
         logoImg = PhotoImage(file = 'logo.PNG')
-        logo = Label(self.titleBar, image=logoImg, bd=0, bg="#222222"); logo.image = logoImg
+        logo = Label(self.titleBar, image=logoImg, bd=0, bg=bgColor); logo.image = logoImg
         """declairing variable for UI ends here"""
 
 
@@ -48,7 +56,7 @@ class Application:
         minImg = PhotoImage(file = "yellow.PNG")
         maxImg = PhotoImage(file = "green.PNG")
         exitbtn = Button(self.topBar, image=exitImg, bd=0, command=self.destructor); exitbtn.image = exitImg
-        maxbtn = Button(self.topBar, image=maxImg, bd=0, command=self.addList); maxbtn.image = maxImg
+        maxbtn = Button(self.topBar, image=maxImg, bd=0); maxbtn.image = maxImg
         minbtn = Button(self.topBar, image=minImg, bd=0, command=self.minimize); minbtn.image = minImg
         """declaring variables for top bar buttons ends here"""
 
@@ -66,23 +74,27 @@ class Application:
         maxbtn.grid(column = 2, row = 0, padx=(1,0))
 
         titleLabel = Label(self.titleBar, text="Cosmetic Quality Defect Detection in Electronics",
-            bd=0, bg="#222222", foreground='#fefefe',font=('verdana', 10))
+            bd=0, bg=bgColor, foreground='#fefefe',font=('verdana', 10))
 
         self.topBar.grid(sticky=(W,E))        
         self.blueLine.grid(row=1,column=0,sticky=(W,E))
         self.titleBar.grid(row=2,column=0,sticky=(W,E))
         logo.grid(row=0, column=0, pady=3); titleLabel.grid(row=0, column=1, padx=10)
         self.centerFrame.grid(row=3, column=0,sticky=(W,E))
-        self.terminalFrame.grid(row=4,column=0,sticky=(N,E,S,W),padx=3,pady=3)
+        self.terminalFrame.grid(row=4, column=0,sticky=(W,E))
+        self.terminalListBox.grid(row=0,column=0,sticky=(N,E,S,W),padx=3,pady=3)
+        self.terminalScrollBar.grid(row=0,column=1,sticky=(N,S,E),padx=(2,3),pady=3)
         self.panel.grid(row=0,column=0,sticky=W,padx=(3,0))
         self.lblFrame.grid(row=0,column=1,sticky=(N,E,S,W),padx=(0,3))
 
 
         # create a button, that when pressed, will take the current frame and save it to file
-        detectbtn = Button(self.lblFrame, text="Run Detection", command=self.take_snapshot)
-        detectbtn.configure(foreground="white", background="#4285F4", width=20, height=2, relief=GROOVE, font=('verdana', 10, 'bold') )
-        detectbtn.grid(column = 0, row = 1, sticky = (E,W))
+        detectionButton = Button(self.lblFrame, text="Run Detection", command=self.take_snapshot)
+        detectionButton.configure(foreground="white", background="#4285F4", width=20, height=2, relief=GROOVE, font=('verdana', 10, 'bold') )
+        detectionButton.grid(column = 0, row = 1, sticky = (E,W))
 
+        changeDirButton = Button(self.lblFrame, text="change saving directory", command=(self.change_saving_dir))
+        changeDirButton.grid(column = 0, row = 3, sticky = (E,W), pady=(375,0))
 
         # create a button, that when pressed, will take the current frame and save it to file
         self.entr = Entry(self.lblFrame, text="evaluate")
@@ -129,6 +141,11 @@ class Application:
             frametk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter
             self.panel.frametk = frametk  # anchor frametk so it does not be deleted by garbage-collector
             self.panel.config(image=frametk,width = 640, height = 480)  # show the image
+        if not ok:
+            if self.counter == 0:
+                self.counter += 1
+                self.terminalPrint("WARNING", "No connected camera found!!!")
+            self.cam = cv2.VideoCapture(0)
         self.root.after(30, self.video_loop)  # call the same function after 30 milliseconds
 
     def take_snapshot(self):
@@ -136,17 +153,21 @@ class Application:
         self.counter += 1
         ts = datetime.datetime.now() # grab the current timestamp
         filename = "{}_Image_{}.jpg".format(ts.strftime("%Y_%m_%d"),self.counter)  # construct filename
-        path = os.path.join(CWD_PATH, 'save_images', filename)  # construct output path
+        path = os.path.join(self.saving_dir, filename)  # construct output path
         image = cv2.cvtColor(numpy.array(self.current_image), cv2.COLOR_RGB2BGR)  # save image as jpeg file
         cv2.imwrite(path, image)
         result = self.entr.get()
         message = "saved! {}".format(filename)
-        self.addList(message)
-        self.addList("Evaluating captured image. Please wait...")
+        self.terminalPrint("INFO",message)
+        self.terminalPrint("INFO","Evaluating captured image. Please wait...")
         self.createWindow()
+    
+    def change_saving_dir(self):
+        self.saving_dir = filedialog.askdirectory()
+        self.terminalPrint("PATH",f" {self.saving_dir}")
 
-    def addList(self,message):
-        self.terminalFrame.insert(self.listCounter, f"[INFO] {message}")
+    def terminalPrint(self, mtype,message):
+        self.terminalListBox.insert(self.listCounter, f"[{mtype}] {message}")
         self.listCounter += 1
 
     def destructor(self):
